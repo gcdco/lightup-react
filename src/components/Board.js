ffa0998d46e9de9143a0c68abf01ff4635406e4f
Caleb Duensing <caleb@calebduensing.com>
2021-04-13 00:36:08 -0600

    progress update.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-import Space from './Space'
+import Space from './Space';
+import board1 from './gameBoards';
+
+/*
+1. Light, Number, White components?
+2. Space component that  handles {Light, Number, White} conditions?
+3. Methods:
+*/
 
 class Board extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { board: board1 }
     }
     render() { 
         return (         
             <div className ='Board'>
                 <h1>Board</h1>
                 <Space />
+                {console.log(this.state.board)}
             </div>
         )
     }
--- src/components/Space.js
+++ src/components/Space.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 
 class Space extends Component {
+    defaultProps = { 
+        light: false,
+        lit: false,
+        number: false,
+        black: false,
+        value: null
+     }
+
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
+
     render() { 
+        const light = <i class="far fa-lightbulb" style={{fontSize: "4em", backgroundColor: '#ffff3f'}}></i>
+
         return ( 
             //<td>cell</td>
-            <h1>Space</h1>
+            <div>
+                <h1>Space</h1>
+                {light}
+            </div>
          );
     }
 }
