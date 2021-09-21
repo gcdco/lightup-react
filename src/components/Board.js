64a4de07ba786f238997c6ded32b814f6ff70bb7
Caleb Duensing <caleb@calebduensing.com>
2021-09-21 00:19:41 -0600

    updated structure. added axios. use effect.. refactored spaces

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,27 +1,17 @@
 import React, { Component } from 'react';
-import Space from './Space';
+import WhiteSpace from './WhiteSpace';
 import board1 from './gameBoards';
 
-/*
-1. Light, Number, White components?
-2. Space component that  handles {Light, Number, White} conditions?
-3. Methods:
-*/
 
-class Board extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { board: board1 }
-    }
-    render() { 
-        return (         
-            <div className ='Board'>
-                <h1>Board</h1>
-                <Space />
-                {console.log(this.state.board)}
-            </div>
-        )
-    }
-}
- 
+const Board = ({ board }) => {
+    console.log(board[0][0]);
+
+    return (
+        <div className='Board'>
+            <h1>Board</h1>
+            <WhiteSpace />
+        </div>
+    );
+};
+
 export default Board;
